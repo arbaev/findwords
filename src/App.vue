a160a158daa51bd5325a7c09066b7d2bd0830bf4
Tim Arbaev <timbrk@gmail.com>
2022-07-05 13:11:47 +0300

    add TrashyString component

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,9 @@
 // TODO: генерация мусора по принципу согласная-гласная
 // TODO: Добавление символа впереди в отдельную функцию (если вообще оно надо)
 import { words } from "./assets/russian-simple-nouns";
+import TrashyString from "./components/TrashyString.vue";
 
-const STR_LEN = 40; // длина строки
+// TODO: вынести все настройки в отдельный файл
 const WORDS_MIN = 1; // мин количество слов в строке
 const WORDS_MAX = 4; // макс количество слов в строке
 
@@ -11,15 +12,15 @@ export default {
   name: "FindWords",
   data() {
     return {
-      str: "bla",
       wordsQuantity: this.getRandomInt(WORDS_MIN, WORDS_MAX),
-      ws: [],
+      words: [],
     };
   },
-  components: {},
+  components: {
+    TrashyString,
+  },
   created() {
-    this.ws = this.getRandWordsArray();
-    this.str = this.strConstructor(this.ws);
+    this.words = this.getRandWordsArray();
   },
   mounted() {},
   methods: {
@@ -36,10 +37,10 @@ export default {
       return words.splice(wordIndex, 1)[0];
     },
 
-    addFirstLetter(arr) {
-      // добавляет первую случайную букву к каждому элементу массива
-      return arr.map((item) => this.randomString(1) + item);
-    },
+    // addFirstLetter(arr) {
+    //   // добавляет первую случайную букву к каждому элементу массива
+    //   return arr.map((item) => this.randomString(1) + item);
+    // },
 
     getRandWordsArray() {
       // возвращает массив выбранных слов
@@ -48,58 +49,7 @@ export default {
       for (let k = 0; k < this.wordsQuantity; k++) {
         arr.push(this.spliceRandWord()); //.toUpperCase());
       }
-      return this.addFirstLetter(arr);
-    },
-
-    randomString(n) {
-      // генерирует строку случайных символов (заглавная кириллица) длиной n
-      let str = "";
-      while (str.length < n)
-        str += String.fromCharCode(Math.random() * 1106).replace(
-          /[^А-ЯЁ]|_/g,
-          ""
-        );
-      return str;
-    },
-
-    howManyLetters(arr) {
-      // суммирует количество букв в массиве строк
-      return arr.reduce((acc, i) => (acc += i.length), 0);
-    },
-
-    strConstructor(wordsArray) {
-      // дополняет массив слов мусорными элементами и перемешивает его
-      while (this.howManyLetters(wordsArray) < STR_LEN - 3) {
-        // заполняем массив рандомом на 3 меньше нужной длины
-        let n = this.getRandomInt(1, 3);
-        wordsArray.push(this.randomString(n));
-      }
-      // дополняем массив до требуемой длины (эта суета нужна, чтобы не вылезти за пределы STR_LEN)
-      let count = this.howManyLetters(wordsArray);
-      if (count < STR_LEN) {
-        wordsArray.push(this.randomString(STR_LEN - count));
-      }
-
-      const wordsArrayRandom = this.shuffle(wordsArray);
-      const ws = this.arrToString(wordsArrayRandom);
-      return ws;
-    },
-
-    shuffle(arr) {
-      // перемешивает элементы массива случайным образом
-      let j, temp;
-      for (let i = arr.length - 1; i > 0; i--) {
-        j = Math.floor(Math.random() * (i + 1));
-        temp = arr[j];
-        arr[j] = arr[i];
-        arr[i] = temp;
-      }
-      return arr;
-    },
-
-    arrToString(arr) {
-      // конвертирует массив строк в строку
-      return arr.reduce((acc, item) => (acc = acc + item), "");
+      return arr; // this.addFirstLetter(arr);
     },
   },
 };
@@ -107,7 +57,7 @@ export default {
 
 <template>
   <main>
-    <h2>{{ str }}</h2>
+    <trashy-string :words="words"></trashy-string>
   </main>
 </template>
 
---
+++ src/components/TrashyString.vue
@@ -0,0 +1,79 @@
+<script>
+const STR_LEN = 40; // длина строки
+
+export default {
+  name: "TrashyString",
+  props: ["words"],
+  data() {
+    return {
+      str: "",
+    };
+  },
+  created() {
+    this.str = this.strConstructor(this.words);
+  },
+  methods: {
+    // TODO: выделить в хелпер?
+    getRandomInt(min, max) {
+      // Получение случайного целого числа в заданном интервале
+      min = Math.ceil(min);
+      max = Math.floor(max);
+      return Math.floor(Math.random() * (max - min + 1)) + min;
+    },
+
+    randomString(n) {
+      // генерирует строку случайных символов (заглавная кириллица) длиной n
+      let str = "";
+      while (str.length < n)
+        str += String.fromCharCode(Math.random() * 1106).replace(
+          /[^А-ЯЁ]|_/g,
+          ""
+        );
+      return str;
+    },
+
+    howManyLetters(arr) {
+      // суммирует количество букв в массиве строк
+      return arr.reduce((acc, i) => (acc += i.length), 0);
+    },
+
+    strConstructor(wordsArray) {
+      // дополняет массив слов мусорными элементами и перемешивает его
+      while (this.howManyLetters(wordsArray) < STR_LEN - 3) {
+        // заполняем массив рандомом на 3 меньше нужной длины
+        let n = this.getRandomInt(1, 3);
+        wordsArray.push(this.randomString(n));
+      }
+      // дополняем массив до требуемой длины (эта суета нужна, чтобы не вылезти за пределы STR_LEN)
+      let count = this.howManyLetters(wordsArray);
+      if (count < STR_LEN) {
+        wordsArray.push(this.randomString(STR_LEN - count));
+      }
+
+      const wordsArrayRandom = this.shuffle(wordsArray);
+      const ws = this.arrToString(wordsArrayRandom);
+      return ws;
+    },
+
+    shuffle(arr) {
+      // перемешивает элементы массива случайным образом
+      let j, temp;
+      for (let i = arr.length - 1; i > 0; i--) {
+        j = Math.floor(Math.random() * (i + 1));
+        temp = arr[j];
+        arr[j] = arr[i];
+        arr[i] = temp;
+      }
+      return arr;
+    },
+
+    arrToString(arr) {
+      // конвертирует массив строк в строку
+      return arr.reduce((acc, item) => (acc = acc + item), "");
+    },
+  },
+};
+</script>
+<template>
+  <h2>{{ str }}</h2>
+</template>
